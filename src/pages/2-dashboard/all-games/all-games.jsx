ddd472a9f44d5921870a0e232497299ee6b9ad8a
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2023-07-31 11:10:05 +0100

    updates

--- src/pages/2-dashboard/all-games/all-games.jsx
+++ src/pages/2-dashboard/all-games/all-games.jsx
@@ -62,27 +62,41 @@ const AllGames = ({ className }) => {
             return (
               <React.Fragment key={idx}>
                 {d?.isGame ? (
-                  <Link  to={`/app/game-home/${d?.id}`} key={idx} className='flex flex-col justify-center text-left cursor-pointer p-4 rounded-2xl bg-green-50  no-underline hover:no-underline capitalize text-base space-y-2 flex-shrink-0 -space-y-2'>
-                    <img className='h-28 w-28 mx-auto object-cover object-top  rounded-t-2xl rounded-b-md' src={d?.photo} onError={e => {
-                      e.target.onerror = null;
-                      e.target.src = Bambi;
-                    }} alt="" />
-                    <div className='flex-shrink-0 w-full space-y-1 bg-green_light rounded-2xl p-3 text-white hover:text-white'>
-                      <div className='font-semibold text-xs line-clamp-1'>{d.title}</div>
-                      <div style={{fontSize: '10px'}} className='text-xs line-clamp-1'>{`Game ${d?.points ? `• ${d?.points} point${d?.points > 1 ? 's' : ''}` : ''}`}</div>
+                  <div key={idx}>
+                    <div className='flex justify-end'>
+                      <div className='bg-yellow-400 p-1 relative right-4 top-1 rounded-t-md font-semibold text-xs'>Game</div>
                     </div>
-                  </Link>
+                    <Link  to={`/app/game-home/${d?.id}`} className={`flex flex-col justify-center text-left cursor-pointer p-4 rounded-2xl bg-green-50  no-underline hover:no-underline capitalize text-base space-y-2 flex-shrink-0 -space-y-2 relative`}>
+                      <div className='h-28 w-28 mx-auto rounded-t-2xl rounded-b-sm'>
+                        <img className='h-full w-full mx-auto object-cover object-top  rounded-t-2xl rounded-b-sm' src={d?.photo} onError={e => {
+                          e.target.onerror = null;
+                          e.target.src = Bambi;
+                        }} alt="" />
+                      </div>
+                      <div className='flex-shrink-0 w-full space-y-1 bg-green_light rounded-2xl p-3 text-white hover:text-white'>
+                        <div className='font-semibold text-xs line-clamp-1'>{d.title}</div>
+                        <div style={{fontSize: '10px'}} className='text-xs line-clamp-1'>{`Game ${d?.points ? `• ${d?.points} point${d?.points > 1 ? 's' : ''}` : ''}`}</div>
+                      </div>
+                    </Link>
+                  </div>
                 ) : (
-                  <Link  to={`/app/trivia-home/${d?.slug}`} key={idx} className='flex flex-col justify-center text-left cursor-pointer p-4 rounded-2xl bg-green-50  no-underline hover:no-underline capitalize text-base space-y-2 flex-shrink-0 -space-y-2'>
-                    <img className='h-28 w-28 mx-auto object-cover object-top  rounded-t-2xl rounded-b-md' src={d?.photo} onError={e => {
-                      e.target.onerror = null;
-                      e.target.src = Bambi;
-                    }} alt="" />
-                    <div className='flex-shrink-0 w-full space-y-1 bg-green_light rounded-2xl p-3 text-white hover:text-white'>
-                      <div className='font-semibold text-xs line-clamp-1'>{d.title}</div>
-                      <div style={{fontSize: '10px'}} className='text-xs line-clamp-1'>Trivia • {d?.trivia_points} point{d?.trivia_points > 1 ? 's' : ''}</div>
+                  <div key={idx}>
+                    <div className='flex justify-end'>
+                      <div className='bg-blue-400 p-1 relative right-4 top-1 rounded-t-md font-semibold text-xs'>Trivia</div>
                     </div>
-                  </Link>
+                    <Link  to={`/app/trivia-home/${d?.slug}`} className={`flex flex-col justify-center text-left cursor-pointer p-4 rounded-2xl bg-green-50  no-underline hover:no-underline capitalize text-base space-y-2 flex-shrink-0 -space-y-2 relative`}>
+                      <div className='h-28 w-28 mx-auto rounded-t-2xl rounded-b-sm'>
+                        <img className='h-full w-full mx-auto object-cover object-top  rounded-t-2xl rounded-b-sm' src={d?.photo} onError={e => {
+                          e.target.onerror = null;
+                          e.target.src = Bambi;
+                        }} alt="" />
+                      </div>
+                      <div className='flex-shrink-0 w-full space-y-1 bg-green_light rounded-2xl p-3 text-white hover:text-white'>
+                        <div className='font-semibold text-xs line-clamp-1'>{d.title}</div>
+                        <div style={{fontSize: '10px'}} className='text-xs line-clamp-1'>Trivia • {d?.trivia_points} point{d?.trivia_points > 1 ? 's' : ''}</div>
+                      </div>
+                    </Link>
+                  </div>
                 )}
               </React.Fragment>
             )
