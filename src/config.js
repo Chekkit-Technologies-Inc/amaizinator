bd8ad9135e86549706b9fd04c9c00ae2c781e35f
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2023-11-30 11:41:27 +0100

    updates

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const apiUrl = 'https://ci-api-v2.chekkit.app/api/v2';
 // export const apiUrlAuth = 'https://ci-api-staging.chekkit.app/api/v2/auth';
 // export const apiUrl = 'https://ci-api-staging.chekkit.app/api/v2';
 
-
+//
 // azurewebsites.net
 // export const apiUrlAuth = 'https://checkkitciprod.azurewebsites.net/api/v2/auth';
 // export const apiUrl = 'https://checkkitciprod.azurewebsites.net/api/v2';
